6445b044c8e8bed84143a24c9c25418607d2b9d7
KevinNoob <hoangviet9768@gmail.com>
2024-06-02 14:56:32 +0700

    init file

---
+++ config.js
@@ -0,0 +1,18 @@
+// 1. Chỉnh sửa các dòng chữ theo mục đích của bạn
+const CONFIG = {
+    introTitle: '🖐 Anh chào Hằng nhớ ! 🖐',
+    introDesc: `Anh đang làm trò con bò và kb em thích không ý,bấm tiếp vào trong đi`,
+    btnIntro: 'Dạ em bấm đây ạ',
+    title: 'Cuối tuần sau đồng ý đi date với a nhé ? 😙',
+    desc: 'Em không trả lời hoặc thoát ra, tức là Em thích Anh đấy  ',
+    btnYes: 'Có chứ em sẽ đi ạ 💖💖💖',
+    btnNo: '💔 Anh mơ à , em không đi vs anh ! 💔 ',
+    question:'Em muốn gặp a lắm đúng k ? 😊😊',
+    reasonPlaceholder: 'Lý do',
+    btnReply: 'Gởi ! 💌💌',
+    reply: 'Vâng ạ em háo hức lắm ý 😊😊',
+    mess: 'Anh biết mà hehe 🥰.',
+    messDesc: ' Hẹn gặp em vào tuần tới ',
+    btnAccept: 'Vâng ả 💖',
+    
+}
